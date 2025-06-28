7aa15902264d754abb3cf984d2e5301e35336257
Aswin337 <aswinkumar11a106.2122@gmail.com>
2025-06-28 19:46:45 +0530

    file added

---
+++ pymysql_db_connect.py
@@ -0,0 +1,33 @@
+import pymysql
+from pymysql.cursors import DictCursor
+connection=pymysql.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="test",
+    cursorclass=DictCursor
+)
+try:
+    with connection.cursor() as cursor:
+        create_query="""Create table if not exists students (
+        id int auto_increment primary key,
+        name varchar(100),
+        age int,
+        department varchar(100)
+        );"""
+        cursor.execute(create_query)
+
+
+        insert_query="""insert into students(name,age,department) values(%s,%s,%s);"""
+        values=[("Anachi",19,"CSE"),("Praveen",20,"MECH"),("Hari",21,"ECE")]
+        cursor.executemany(insert_query,values)
+        connection.commit()
+
+        
+        select_query="""select * from students;"""
+        cursor.execute(select_query)
+        res=cursor.fetchall()
+        for row in res:
+            print(row)
+finally:
+    connection.close()
